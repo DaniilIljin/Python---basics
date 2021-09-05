0549dbb46e7aa97fc9733a5f8160a3880f9b8aac
Daniil <dailji@ttu.ee>
2021-09-05 17:22:48 +0300

    My new commit

--- EX/ex01_hello/cashier.py
+++ EX/ex01_hello/cashier.py
@@ -1,8 +1,8 @@
-"""EX01_cashier."""
-entered_ammount_of_money = int(input("Enter a sum: "))
-remnant1 = entered_ammount_of_money % 50 # I will find a remnant from each division(50,20,10,5)
+"""This is an exercise "cashier" from EX01."""
+entered_amount_of_money_in_cents = int(input("Enter a sum: "))
+remnant1 = entered_amount_of_money_in_cents % 50 # I will find a remnant from each division(50,20,10,5)
 remnant2 = remnant1 % 20
 remnant3 = remnant2 % 10
 remnant4 = remnant3 % 5
 # In the answer I will find the integer after each division and sum them up (greedy algorithm)
-print(f'Amount of coins needed: {(entered_ammount_of_money//50) + (remnant1//20) + (remnant2//10) + (remnant3//5) + (remnant4//1)}')
+print(f'Amount of coins needed: {(entered_amount_of_money_in_cents // 50) + (remnant1 // 20) + (remnant2 // 10) + (remnant3 // 5) + (remnant4 // 1)}')
