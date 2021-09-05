1531c347c6bb856c624b769487f5c9f2a5a62d5c
Daniil <dailji@ttu.ee>
2021-09-05 16:18:16 +0300

    My new commit

--- EX/ex01_hello/cashier.py
+++ EX/ex01_hello/cashier.py
@@ -1,7 +1,8 @@
 """EX01_cashier."""
-a = int(input("Enter a sum: "))
-b = a % 50
-c = b % 20
-d = c % 10
-e = d % 5
-print(f'Amount of coins needed: {(a//50) + (b//20) + (c//10) + (d//5) + (e//1)}')
+entered_ammount_of_money = int(input("Enter a sum: "))
+remnant1 = entered_ammount_of_money % 50 # I will find a remnant from each division(50,20,10,5)
+remnant2 = remnant1 % 20
+remnant3 = remnant2 % 10
+remnant4 = remnant3 % 5
+# In the answer I will find the integer after each division and sum them up (greedy algorithm)
+print(f'Amount of coins needed: {(entered_ammount_of_money//50) + (remnant1//20) + (remnant2//10) + (remnant3//5) + (remnant4//1)}')
