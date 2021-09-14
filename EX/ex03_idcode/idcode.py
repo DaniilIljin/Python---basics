27824c2e4f9aa7feb59eb027f3cd5f5f982e2bcb
Daniil <dailji@ttu.ee>
2021-09-14 12:47:05 +0300

    Today is a good day! (0)

---
+++ EX/ex03_idcode/idcode.py
@@ -0,0 +1,18 @@
+"""EX03 - ID-code."""
+
+
+def find_id_code(text: str):
+    """description."""
+    id = ""
+    count = 0
+    for i in text:
+        if i.isdigit():
+            id += i
+            count += 1
+    if count > 11:
+        return "Too many numbers!"
+    elif count < 11:
+        return "Not enough numbers!"
+    else:
+        return id
+
