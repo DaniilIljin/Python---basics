e230e2448615e1a47ee15d37ef7f5f04fc8092e0
Daniil <dailji@ttu.ee>
2021-09-14 23:17:54 +0300

    Today is a good day! (2)

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -90,7 +90,7 @@ def is_leap_year(year: int) -> bool:
     if year % 4 != 0:
         return False
     elif year % 100 != 0:
-        return False
+        return True
     elif year % 400 != 0:
         return False
     else:
