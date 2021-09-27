9d200f26368971b4aa2675252d5bb94819372925
Daniil <dailji@ttu.ee>
2021-09-27 14:23:59 +0300

    Where am I wrong???

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -4,7 +4,7 @@ import string
 import random
 
 data_type_dictionary = {
-    'string': list(string.ascii_letters),
+    'string': list(string.ascii_lowercase),
     'int': list(range(0, 1000)),
     'float': list(x * 0.5 for x in range(0, 1000)),
     'list': list(list(i) for i in string.ascii_letters),
@@ -112,22 +112,23 @@ if __name__ == '__main__':
     # The given outputs are only some of possible outputs, for example for (3, 'string')
     # in the first part an output of ["kass", "koer", "kana"] would also work.
 
-    # Part 1
-    print(generate_list(2, 'set'))  # [set(), set()]
-    print(generate_list(3, 'string'))  # ["a", "kass", "a"]
-    print(generate_list(1, 'list'))  # [[]]
-    print(generate_list(5, 'int'))  # [1, 2, 3, 3, 3]
-    print()
-
-    # Part 2
-    print(generate_combined_list([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
-    print(generate_combined_list([(3, 'int'), (5, 'int')]))  # [0, 0, 0, 0, 0]
-    print(generate_combined_list([(3, 'int'), (5, 'int'), (4, 'int')]))  # [100, 80, 60, 40, 20]
-    print(generate_combined_list([(2, 'list'), (3, 'string')]))  # ["a", [], "a", [], "a"]
-    print(generate_combined_list([(2, 'float'), (3, 'dict')]))  # [{}, {}, {}, 3.14, 3.15]
+    # # Part 1
+    # print(generate_list(2, 'set'))  # [set(), set()]
+    # print(generate_list(3, 'string'))  # ["a", "kass", "a"]
+    # print(generate_list(1, 'list'))  # [[]]
+    # print(generate_list(5, 'int'))  # [1, 2, 3, 3, 3]
+    # print()
+    #
+    # # Part 2
+    # print(generate_combined_list([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
+    # print(generate_combined_list([(3, 'int'), (5, 'int')]))  # [0, 0, 0, 0, 0]
+    # print(generate_combined_list([(3, 'int'), (5, 'int'), (4, 'int')]))  # [100, 80, 60, 40, 20]
+    # print(generate_combined_list([(2, 'list'), (3, 'string')]))  # ["a", [], "a", [], "a"]
+    # print(generate_combined_list([(2, 'float'), (3, 'dict')]))  # [{}, {}, {}, 3.14, 3.15]
     print()
 
     # Part 3
+    print(generate_combined_list_unique([(3, 'string')]))
     print(generate_combined_list_unique([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
     print(generate_combined_list_unique([(2, 'int'), (2, 'float'), (1, 'int')]))  # [43, 93, 4.3, 2.1]
     print()
