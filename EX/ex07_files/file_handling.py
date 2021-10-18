8e477c86a54926e2fc1b8524f118d7c7db615c43
Daniil <dailji@ttu.ee>
2021-10-18 19:50:57 +0300

    Where am I wrong???

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -2,6 +2,7 @@
 
 import csv
 import datetime
+import glob
 import re
 
 
@@ -468,3 +469,105 @@ def read_csv_file_into_list_of_dicts_using_datatypes(filename: str) -> list:
                 element[element.index(element1)] = 'nothing'
             big_list.append(new_dict)
     return big_list
+
+
+def read_people_data(directory: str) -> dict:
+    """
+    Read people data from files.
+    Files are inside directory. Read all *.csv files.
+
+    Each file has an int field "id" which should be used to merge information.
+
+    The result should be one dict where the key is id (int) and value is
+    a dict of all the different values across the the files.
+    Missing keys should be in every dictionary.
+    Missing value is represented as None.
+
+    File: a.csv
+    id,name
+    1,john
+    2,mary
+    3,john
+
+    File: births.csv
+    id,birth
+    1,01.01.2001
+    2,05.06.1990
+
+    File: deaths.csv
+    id,death
+    2,01.02.2020
+    1,-
+
+    Becomes:
+    {
+        1: {"id": 1, "name": "john", "birth": datetime.date(2001, 1, 1), "death": None},
+        2: {"id": 2, "name": "mary", "birth": datetime.date(1990, 6, 5),
+            "death": datetime.date(2020, 2, 1)},
+        3: {"id": 3, "name": "john", "birth": None, "death": None},
+    }
+
+
+    :param directory: Directory where the csv files are.
+    :return: Dictionary with id as keys and data dictionaries as values.
+    """
+    list_of_all_data = []
+    for file in glob.glob(directory + '/*.csv'):
+        list_of_all_data.extend(read_csv_file_into_list_of_dicts_using_datatypes(file))
+    all_ids = []
+    for dict1 in list_of_all_data:
+        if 'id' in dict1:
+            all_ids.append(dict1['id'])
+    big_dicts = []
+    for id_ in all_ids:
+        connected_dict = {}
+        for dict2 in list_of_all_data:
+            if id_ in dict2.values():
+                connected_dict.update(dict2)
+        big_dicts.append(connected_dict)
+    new_dict = {}
+    for dict2 in big_dicts:
+        new_dict[dict2['id']] = dict2
+    return new_dict
+
+
+def generate_people_report(person_data_directory: str, report_filename: str) -> None:
+    """
+    Generate report about people data.
+
+    Data should be read using read_people_data().
+
+    The input files contain fields "birth" and "death" which are dates. Those can be in different files. There are no duplicate headers in the files (except for the "id").
+
+    The report is a CSV file where all the fields are written to
+    (along with the headers).
+    In addition, there should be two fields:
+    - "status" this is either "dead" or "alive" depending on whether
+    there is a death date
+    - "age" - current age or the age when dying.
+    The age is calculated as full years.
+    Birth 01.01.1940, death 01.01.2020 - age: 80
+    Birth 02.01.1940, death 01.01.2020 - age: 79
+
+    If there is no birth date, then the age is -1.
+
+    When calculating age, dates can be compared.
+
+    The lines in the files should be ordered:
+    - first by the age ascending (younger before older);
+      if the age cannot be calculated, then those lines will come last
+    - if the age is the same, then those lines should be ordered
+      by birthdate descending (newer birth before older birth)
+    - if both the age and birth date are the same,
+      then by name ascending (a before b). If name is not available, use "" (people with missing name should be before people with  name)
+    - if the names are the same or name field is missing,
+      order by id ascending.
+
+    Dates in the report should in the format: dd.mm.yyyy
+    (2-digit day, 2-digit month, 4-digit year).
+
+    :param person_data_directory: Directory of input data.
+    :param report_filename: Output file.
+    :return: None
+    """
+    pass
