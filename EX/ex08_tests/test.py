b24390ae6cc32a37e152ed1a6c72392cfa930632
Daniil <dailji@ttu.ee>
2021-10-21 12:54:55 +0300

    Where am I wrong???

--- EX/ex08_tests/test.py
+++ EX/ex08_tests/test.py
@@ -10,8 +10,8 @@ def test_part1_int_correct_len():
 
 
 def test_part1_correct_type():
-    types = [str, int, set, dict, tuple, list]
-    inputed_types = ['string', 'int', 'set', 'dict', 'tuple', 'list']
+    types = [str, int, set, dict, tuple, list, float]
+    inputed_types = ['string', 'int', 'set', 'dict', 'tuple', 'list', 'float']
     for index, type_ in enumerate(types):
         actual_type = inputed_types[index]
         answer = solution.generate_list(1, actual_type)
@@ -19,7 +19,7 @@ def test_part1_correct_type():
 
 
 def test_part2_minimal_possible_length():
-    for type_ in ['string', 'int', 'set', 'dict', 'tuple', 'list']:
+    for type_ in ['string', 'int', 'set', 'dict', 'tuple', 'list', 'float']:
         answer = solution.generate_combined_list([(3, type_), (5, type_), (4, type_)])
         assert len(answer) == 5
 
