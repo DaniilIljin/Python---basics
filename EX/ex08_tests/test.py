ed6f08fb1b9e223d3e004de90ba46771351d10fc
Daniil <dailji@ttu.ee>
2021-10-21 23:31:50 +0300

    Where am I wrong???

--- EX/ex08_tests/test.py
+++ EX/ex08_tests/test.py
@@ -36,7 +36,7 @@ def test_par2_wrong_types():
         counter += 1
 
 
-def test_part3_size():
+def test_part3_testing_lenght():
     for type_ in ['string', 'int', 'set', 'dict', 'tuple', 'list', 'float']:
         for size in range(100):
             answer = solution.generate_combined_list_unique([(size, type_)])
