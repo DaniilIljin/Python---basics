11acd02d7760d26af78f58189d77dca532294a23
Daniil <dailji@ttu.ee>
2021-10-23 17:15:47 +0300

    Where am I wrong???

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -1,1 +1,15 @@
 """Ex08 testing."""
+import solution
+
+
+def test_students_study_right_time_for_coffee():
+    for time in range(1, 25):
+        if 1 <= time <= 4:
+            assert solution.students_study(time, False) is True
+            assert solution.students_study(time, True) is False
+        elif 5 <= time <= 17:
+            assert solution.students_study(time, True) is True
+            assert solution.students_study(time, False) is False
+        elif 18 <= time <= 24:
+            assert solution.students_study(time, True) is True
+            assert solution.students_study(time, False) is True
