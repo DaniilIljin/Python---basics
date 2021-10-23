b22c4a3c80955c33c394b6dcdae63d50e4c589dc
Daniil <dailji@ttu.ee>
2021-10-23 19:25:28 +0300

    Where am I wrong???

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -21,15 +21,19 @@ def test_lottery_testing_fives():
     assert solution.lottery(5, 5, 5) == 10
 
 
-def test_lottery_testing_similar():
-    """Test numbers in range(100)."""
-    for num in range(1, 30):
-        assert solution.lottery(num, num, num) == 5
-
-
 def test_lottery_testing_testing_diff_a_b_c():
     for num in range(100):
         a = num
         b = num + 1
         c = num + 2
         assert solution.lottery(a, b, c) == 1
+
+
+def test_lottery_testing_zero():
+    assert solution.lottery(0, 0, 0) == 5
+
+
+# def test_lottery_testing_similar():
+#     """Test numbers in range(100)."""
+#     for num in range(1, 30):
+#         assert solution.lottery(num, num, num) == 5
