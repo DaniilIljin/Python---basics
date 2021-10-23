7c0176605d5ecb2937302e2a10effdebd56b0059
Daniil <dailji@ttu.ee>
2021-10-23 18:45:04 +0300

    Where am I wrong???

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -6,7 +6,7 @@ def test_students_study_right_time_for_coffee():
     """Test the right time for coffee."""
     for time in range(1, 25):
         if 1 <= time <= 4:
-            assert solution.students_study(time, False) is True
+            assert solution.students_study(time, False) is False
             assert solution.students_study(time, True) is False
         elif 5 <= time <= 17:
             assert solution.students_study(time, True) is True
@@ -14,4 +14,3 @@ def test_students_study_right_time_for_coffee():
         else:
             assert solution.students_study(time, True) is True
             assert solution.students_study(time, False) is True
-    assert solution.students_study(1, False)
