f0b3d221dea9cf91052e7a43125340464bb2dbc2
Daniil <dailji@ttu.ee>
2021-10-23 19:19:23 +0300

    Where am I wrong???

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -17,13 +17,15 @@ def test_students_study_right_time_for_coffee():
 
 
 def test_lottery_testing_fives():
+    """Test all fives."""
     assert solution.lottery(5, 5, 5) == 10
 
 
-# def test_lottery_testing_similar():
-#     for num in range(100):
-#         assert solution.lottery(num, num, num) == 5
-#
+def test_lottery_testing_similar():
+    """Test numbers in range(100)."""
+    for num in range(100):
+        assert solution.lottery(num, num, num) == 5
+
 #
 # def test_lottery_testing_testing_diff_a_b_c():
 #     for num in range(100):
