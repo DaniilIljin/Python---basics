2faded9784d511e5f8e2ba22e95483a1ba0a0dd4
Daniil <dailji@ttu.ee>
2021-10-24 12:41:57 +0300

    Where am I wrong???

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -73,68 +73,85 @@ def test_fruit_order__only_big_more_than_required_no_match():
 
 
 def test_fruit_order__only_big_exact_match():
+    """O."""
     assert solution.fruit_order(0, 2, 10) == 0
 
 
 def test_fruit_order__only_small_exact_match():
+    """O."""
     assert solution.fruit_order(5, 0, 5) == 5
 
 
 def test_fruit_order__zero_amount_zero_small():
+    """O."""
     assert solution.fruit_order(0, 5, 0) == 0
 
 
 def test_fruit_order__zero_amount_zero_big():
+    """O."""
     assert solution.fruit_order(5, 0, 0) == 0
 
 
 def test_fruit_order__zero_amount_others_not_zero():
+    """O."""
     assert solution.fruit_order(5, 6, 0) == 0
 
 
 def test_fruit_order__only_big_not_enough_but_multiple_of_5():
+    """O."""
     assert solution.fruit_order(0, 3, 20) == -1
 
 
 def test_fruit_order__only_big_more_than_required_match():
+    """O."""
     assert solution.fruit_order(0, 6, 20) == 0
 
 
 def test_fruit_order__only_small_match_more_than_5_smalls():
+    """O."""
     assert solution.fruit_order(6, 0, 6) == 6
 
 
 def test_fruit_order__only_small_not_enough_more_than_5_smalls():
+    """O."""
     assert solution.fruit_order(6, 0, 7) == -1
 
 
 def test_fruit_order__only_small_more_than_required():
+    """O."""
     assert solution.fruit_order(7, 0, 6) == 6
 
 
 def test_fruit_order__all_positive_exact_match():
+    """O."""
     assert solution.fruit_order(7, 3, 22) == 7
 
 
 def test_fruit_order__use_all_smalls_some_bigs():
+    """O."""
     assert solution.fruit_order(2, 5, 22) == 2
 
 
 def test_fruit_order__use_some_smalls_all_bigs():
+    """O."""
     assert solution.fruit_order(7, 4, 22) == 2
 
 
 def test_fruit_order__use_some_smalls_some_bigs():
+    """O."""
     assert solution.fruit_order(7, 8, 22) == 2
 
 
 def test_fruit_order__enough_bigs_not_enough_smalls():
+    """O."""
     assert solution.fruit_order(1, 5, 22) == -1
 
 
 def test_fruit_order__not_enough_with_more_than_5_smalls():
+    """O."""
     assert solution.fruit_order(6, 3, 22) == -1
 
 
 def test_fruit_order__match_large_numbers():
-    assert solution.fruit_order(50, 27, 185) == 50
+    """O."""
+    assert solution.fruit_order(500, 1000, 5500) == 500
