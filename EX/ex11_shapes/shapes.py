0b88f391dec9d4197568de4a3146bda59c7e677f
Daniil <dailji@ttu.ee>
2021-11-08 18:19:24 +0200

    Where am I wrong???

---
+++ EX/ex11_shapes/shapes.py
@@ -0,0 +1,163 @@
+"""Shapes."""
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    """General shape class."""
+
+    def __init__(self, color: str):
+        """Constructor, sets the color."""
+        self.color = color
+
+    def set_color(self, color: str):
+        """Set the color of the shape."""
+        self.color = color
+
+    def get_color(self) -> str:
+        """Get the color of the shape."""
+        return self.color
+
+    @abstractmethod
+    def get_area(self) -> float:
+        """Get area method which every subclass has to override."""
+        print("Implement area")
+
+
+class Circle(Shape):
+    """Circle is a subclass of Shape."""
+
+    def __init__(self, color: str, radius: float):
+        """
+        Constructor of the circle.
+
+        The color is stored using superclass constructor:
+        super().__init__(color)
+
+        The radius value is stored here.
+        """
+        super().__init__(color)
+        a = self.color
+        self.radius = radius
+
+    def __repr__(self) -> str:
+        """
+        Return representation of the circle.
+
+        For this exercise, this should return a string:
+        Circle (r: {radius}, color: {color})
+        """
+        return f'r: {self.radius}, color: {self.color}'
+
+    def get_area(self) -> float:
+        """
+        Calculate the area of the circle.
+
+        Area of the circle is pi * r * r.
+        """
+        return self.radius ** 2 * 3.14
+
+
+class Square(Shape):
+    """Square is a subclass of Shape."""
+
+    def __init__(self, color: str, side: float):
+        """
+        Constructor of the square.
+
+        The color is stored using superclass constructor:
+        super().__init__(color)
+
+        The side value is stored here.
+        """
+        super().__init__(color)
+        self.side = side
+
+    def __repr__(self) -> str:
+        """
+        Return representation of the square.
+
+        For this exercise, this should return a string:
+        Square (a: {side}, color: {color})
+        """
+        return f'a: {self.side}, color: {self.color}'
+
+    def get_area(self) -> float:
+        """
+        Calculate the area of the square.
+
+        Area of the square is side * side.
+        """
+        return self.side ** 2
+
+
+class Rectangle(Shape):
+    """Rectangle is a subclass of Shape."""
+
+    def __init__(self, color: str, length: float, width: float):
+        """
+        Constructor of the rectangle.
+
+        The color is stored using superclass constructor:
+        super().__init__(color)
+
+        Length and width values are stored here.
+        """
+        super().__init__(color)
+        self.length = length
+        self.width = width
+
+    def __repr__(self) -> str:
+        return f'l: {self.length}, w: {self.width}, color: {self.color}'
+
+    def get_area(self) -> float:
+        """
+        Calculate the area of the rectangle.
+
+        Area of the rectangle is length * width.
+        """
+        return self.length * self.width
+
+
+class Paint:
+    """The main program to manipulate the shapes."""
+
+    def __init__(self):
+        """Constructor should create a list to store all the shapes."""
+        self.list = []
+
+    def add_shape(self, shape: Shape) -> None:
+        """Add a shape to the program."""
+        self.list += [shape]
+
+    def get_shapes(self) -> list:
+        """Return all the shapes."""
+        return self.list
+
+    def calculate_total_area(self) -> float:
+        """Calculate total area of the shapes."""
+        total_area = 0
+        for shape in self.list:
+            total_area += shape.get_area()
+        return total_area
+
+    def get_circles(self) -> list:
+        """Return only circles."""
+        return [shape for shape in self.list if type(shape) is Circle]
+
+    def get_squares(self) -> list:
+        """Return only squares."""
+        return [shape for shape in self.list if type(shape) is Square]
+
+    def get_rectangles(self) -> list:
+        """Return only rectangles."""
+        return [shape for shape in self.list if type(shape) is Rectangle]
+
+
+if __name__ == '__main__':
+    paint = Paint()
+    circle = Circle("blue", 10)
+    square = Square("red", 11)
+    paint.add_shape(circle)
+    paint.add_shape(square)
+    print(paint.calculate_total_area())
+    print(paint.get_circles())
