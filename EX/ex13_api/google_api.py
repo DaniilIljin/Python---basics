7278b73903bcec78d043740afbf34f76cf6bc83c
Daniil <dailji@ttu.ee>
2021-11-24 14:18:04 +0200

    Where am I wrong???

--- EX/ex13_api/google_api.py
+++ EX/ex13_api/google_api.py
@@ -1,4 +1,7 @@
 """EX13."""
+from __future__ import print_function
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
 
 
 def get_links_from_spreadsheet(id: str, token: str) -> list:
@@ -10,7 +13,22 @@ def get_links_from_spreadsheet(id: str, token: str) -> list:
     Returns
         ['Student Name', 'Alexandra', 'Andrew', 'Anna', 'Becky', ... and so on from the first column]
     """
-    return []
+    # If modifying these scopes, delete the file token.json.
+    scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+    # The ID and range of a sample spreadsheet.
+    sample_spreadsheet_id = id
+
+    creds = Credentials.from_authorized_user_file(token, scopes)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=id,
+                                range='A:A').execute()
+    values = result.get('values', [])
+    return values
 
 
 def get_links_from_playlist(link: str, developer_key: str) -> list:
@@ -19,8 +37,10 @@ def get_links_from_playlist(link: str, developer_key: str) -> list:
     Example input
         get_links_from_playlist('https://www.youtube.com/playlist?list=PLFt_AvWsXl0ehjAfLFsp1PGaatzAwo0uK',
                                 'ThisIsNotARealKey_____ThisIsNotARealKey')
-
     Returns
         ['https://youtube.com/watch?v=r_It_X7v-1E', 'https://youtube.com/watch?v=U4ogK0MIzqk', ... and so on]
     """
     return []
+
+
+print(get_links_from_spreadsheet('1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms', 'token.json'))
