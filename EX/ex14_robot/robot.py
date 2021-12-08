b2f99ad1a5e85f9f2f8f9a22a515f2a2f48a1df1
Daniil <dailji@ttu.ee>
2021-12-08 14:40:35 +0200

    Where am I wrong???

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -23,15 +23,15 @@ def drive_to_line(robot: FollowerBot):
     :param FollowerBot robot: instance of the robot that you need to make move
     """
 
-    while True:
-        if robot.get_line_sensors() == 0:
-            robot.set_wheels_speed(50)
-            robot.sleep(0.01)
-            robot.done()
-            break
-        else:
-            robot.set_wheels_speed(50)
-            robot.sleep(2)
+    # while True:
+    #     if robot.get_line_sensors() == 0:
+    #         robot.set_wheels_speed(50)
+    #         robot.sleep(0.01)
+    #         robot.done()
+    #         break
+    #     else:
+    #         robot.set_wheels_speed(50)
+    #         robot.sleep(2)
 
 
 def follow_the_line(robot: FollowerBot):
