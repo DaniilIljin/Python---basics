d98f3826adeda11a7cb24534cc7f77b08fcb8e78
Daniil <dailji@ttu.ee>
2021-12-08 14:37:59 +0200

    Where am I wrong???

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -9,7 +9,7 @@ def test_run(robot: FollowerBot):
     :param FollowerBot robot: instance of the robot that you need to make move
     """
     robot.set_wheels_speed(50)
-    robot.sleep(2)
+    robot.sleep(0.01)
     robot.set_wheels_speed(6)
     robot.done()
 
@@ -22,7 +22,16 @@ def drive_to_line(robot: FollowerBot):
 
     :param FollowerBot robot: instance of the robot that you need to make move
     """
-    pass
+
+    while True:
+        if robot.get_line_sensors() == 0:
+            robot.set_wheels_speed(50)
+            robot.sleep(0.01)
+            robot.done()
+            break
+        else:
+            robot.set_wheels_speed(50)
+            robot.sleep(2)
 
 
 def follow_the_line(robot: FollowerBot):
@@ -43,3 +52,5 @@ def the_true_follower(robot: FollowerBot):
     :param FollowerBot robot: instance of the robot that you need to make move
     """
     pass
+
+drive_to_line(FollowerBot())
